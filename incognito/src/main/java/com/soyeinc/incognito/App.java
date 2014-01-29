207f5309acb6224ac6b45c28948f2ddfc38d21ff
lilsoye4u <lilsoye4u@gmail.com>
2014-01-28 19:51:49 -0500

    genesis

---
+++ incognito/src/main/java/com/soyeinc/incognito/App.java
@@ -0,0 +1,13 @@
+package com.soyeinc.incognito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
